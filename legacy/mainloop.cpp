c63a2a6c1d69606a01ef01b4fed4f048c90400d5
jkasper <jkasper@student.42heilbronn.de>
2022-07-08 15:49:45 +0200

    init update

--- legacy/mainloop.cpp
+++ legacy/mainloop.cpp
@@ -1,4 +1,4 @@
-#include <raylib.h>
+#include "raylib.h"
 #include <iostream>
 #include "unit.hpp"
 
--- legacy/unit.cpp
+++ legacy/unit.cpp
@@ -1,6 +1,6 @@
 #include "unit.hpp"
 #include <string>
-#include <raylib.h>
+#include "raylib.h"
 #include <iostream> 
 
 unit::unit(int nhp, int nrange, int nmove, int nad) : _max_hp(nhp), _hp(nhp), _range(nrange), _move(nmove), _ad(nad), _moral(100), _multip(1) {}
